4ed957e63eed30b82b7ded54bf463d93fdcc056e
jtechmind <jtechmind@gmail.com>
2025-01-31 23:58:58 +0530

    feat: Successfully set up eStore Eureka Server and registered microservices
    - eStore Eureka Server is running successfully
    - E-Store microservices are registered and using it
    - Verified service discovery and communication between microservices

---
+++ src/main/java/com/jtechmind/estore_eureka_server/EstoreEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.jtechmind.estore_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoreEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtechmind/estore_eureka_server/EstoreEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtechmind.estore_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoreEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
